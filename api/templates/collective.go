3b0fade1d0b429c77d4c7f173fe38e9b79442552
Ruben M. Damião <rmdamiao@gmail.com>
2023-06-15 18:46:36 -0300

    template

--- api/templates/collective.go
+++ api/templates/collective.go
@@ -6,6 +6,17 @@ import (
 	"text/template"
 )
 
+type Collectives struct {
+	Collectives []Collective
+}
+
+type Collective struct {
+	Name          string
+	Description   string
+	Majority      int
+	SuperMajority int
+}
+
 func main() {
 	// Define a template.
 	const collectiveHTML = `
@@ -16,6 +27,9 @@ func main() {
 			<title>Collective</title>
 		</head>
 		<body>
+			<ul>
+{{range .Collectives}}
+<li>
 			<h1>{{.Name}} Collective</h1>
 			<br/>
 			<h2>Description</h2>
@@ -25,31 +39,25 @@ func main() {
 			<p>{{.Majority}}</p>
 			<h2>Super Majority Policy</h2>
 			<p>{{.SuperMajority}}</p>
+</li>
+{{end}}
+			</ul>
 		</body>
 	</html>
 `
 
-	// Prepare some data to insert into the template.
-	type Collective struct {
-		Name          string
-		Description   string
-		Majority      int
-		SuperMajority int
-	}
-	var collective = []Collective{
+	collectives := Collectives{Collectives: []Collective{
 		{"First Collective", "first description", 10, 50},
 		{"Random Collective", "random description", 100, 100},
 		{"Default Collective", "default description", 1, 20},
-	}
+	}}
 
 	// Create a new template and parse the letter into it.
 	t := template.Must(template.New("letter").Parse(collectiveHTML))
 
 	// Execute the template for each recipient.
-	for _, r := range collective {
-		err := t.Execute(os.Stdout, r)
-		if err != nil {
-			log.Println("executing template:", err)
-		}
+	err := t.Execute(os.Stdout, collectives)
+	if err != nil {
+		log.Println("executing template:", err)
 	}
 }
