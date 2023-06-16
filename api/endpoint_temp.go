0d8c07d854edb9783236a91c1407d0ade380578a
lienko <larigdtk@gmail.com>
2023-06-16 19:41:05 -0300

    including boards, events and draft templates to endpoin

--- api/endpoint_temp.go
+++ api/endpoint_temp.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"crypto"
 	"log"
 	"net/http"
 	"text/template"
@@ -8,6 +9,60 @@ import (
 
 	"github.com/lienkolabs/synergy/social/state"
 )
+
+// Boards template struct
+
+type BoardsView struct {
+	Name        string
+	Description string
+	Keywords    []string
+	PinMajority int
+}
+
+type BoardsListView struct {
+	Boards []BoardsView
+}
+
+type BoardDetailView struct {
+	Name        string
+	Description string
+	Collective  string
+	Keywords    []string
+	PinMajority int
+	Editors     []string
+}
+
+func BoardsFromState(state *state.State) BoardsListView {
+	view := BoardsListView{
+		Boards: make([]BoardsView, 0),
+	}
+	for _, board := range state.Boards {
+		itemView := BoardsView{
+			Name:        board.Name,
+			Description: board.Description,
+			Keywords:    board.Keyword,
+			PinMajority: board.Editors.Majority,
+		}
+		view.Boards = append(view.Boards, itemView)
+	}
+	return view
+}
+
+func BoardsDetailFromState(state *state.State, name string) *BoardDetailView {
+	board, ok := state.Board(name)
+	if !ok {
+		return nil
+	}
+	view := BoardDetailView{
+		Name:        board.Name,
+		Description: board.Description,
+		Collective:  board.Collective.Name,
+		Keywords:    board.Keyword,
+		PinMajority: board.Editors.Majority,
+		Editors:     board.Editors.Members.Handle,
+	}
+	return &view
+}
 
 // Collectives template struct
 
@@ -51,6 +106,126 @@ func CollectiveDetailFromState(state *state.State, name string) *CollectiveDetai
 		Description:   collective.Description,
 		Majority:      collective.Policy.Majority,
 		SuperMajority: collective.Policy.SuperMajority,
+	}
+	return &view
+}
+
+// Drafts template struct
+
+type DraftsView struct {
+	Title       string
+	Author      string
+	CoAuthors   []string
+	Description string
+	Keywords    []string
+}
+
+type DraftsListView struct {
+	Drafts []DraftsView
+}
+
+type DraftDetailView struct {
+	Title        string
+	Description  string
+	Keywords     []string
+	Content      string
+	Author       string
+	CoAuthors    []string
+	References   []string
+	PreviousHash crypto.Hash
+}
+
+func DraftsFromState(state *state.State) DraftsListView {
+	view := DraftsListView{
+		Drafts: make([]DraftsView, 0),
+	}
+	for _, draft := range state.Drafts {
+		itemView := DraftsView{
+			Title:        draft.Title,
+			Description:  draft.Description,
+			Keywords:     draft.Keywords,
+			Content:      draft.Content,
+			Author:       draft.Author,
+			CoAuthors:    draft.Authors,
+			References:   draft.References,
+			PreviousHash: draft.PreviousVersion,
+		}
+		view.Collectives = append(view.Collectives, itemView)
+	}
+	return view
+}
+
+func DraftDetailFromState(state *state.State, title string) *DraftDetailView {
+	draft, ok := state.Drafts(title)
+	if !ok {
+		return nil
+	}
+	view := DraftDetailView{
+		Title:       draft.Title,
+		Author:      draft.Author,
+		CoAuthors:   draft.CoAuthors,
+		Description: draft.Description,
+		Keywords:    draft.Keywords,
+	}
+	return &view
+}
+
+// Events template struct
+
+type EventsView struct {
+	ID          string
+	Description string
+	Collective  string
+	Public      bool
+	StartAt     time.Time
+}
+
+type EventsListView struct {
+	Events []EventsView
+}
+
+type EventDetailView struct {
+	StartAt      time.Time
+	Description  string
+	Collective   string
+	EstimatedEnd time.Time
+	Venue        string
+	Open         bool
+	Public       bool
+	Managers     []string
+}
+
+func EventsFromState(state *state.State) EventsListView {
+	view := EventsListView{
+		Events: make([]EventsView, 0),
+	}
+	for _, event := range state.Events {
+		itemView := EventsView{
+			ID:          event.ID,
+			Description: event.Description,
+			Collective:  event.Collective.Name,
+			Public:      event.Public,
+			StartAt:     event.StartAt,
+		}
+		view.Events = append(view.Events, itemView)
+	}
+	return view
+}
+
+func EventDetailFromState(state *state.State, ID string) *EventDetailView {
+	event, ok := state.Events(ID)
+	if !ok {
+		return nil
+	}
+	view := EventDetailView{
+		StartAt:      event.StartAt,
+		Description:  event.Description,
+		Collective:   event.Collective.Name,
+		EstimatedEnd: event.EstimatedEnd,
+		Venue:        event.Venue,
+		Open:         event.Open,
+		Public:       event.Public,
+		Managers:     event.Managers,
 	}
 	return &view
 }
@@ -109,70 +284,23 @@ func MemberDetailFromState(state *state.State, hash string) *MemberDetailView {
 	return &view
 }
 
-// Events template struct
-
-type EventsView struct {
-	ID          string
-	Description string
-	Owner       string
-	Public      bool
-	StartAt     time.Time
-}
-
-type EventsListView struct {
-	Events []EventsView
-}
-
-type EventDetailView struct {
-	StartAt      time.Time
-	Description  string
-	Collective   string
-	EstimatedEnd time.Time
-	Venue        string
-	Open         bool
-	Public       bool
-	Managers     []string
-}
-
-func EventsFromState(state *state.State) EventsListView {
-	view := EventsListView{
-		Events: make([]EventsView, 0),
-	}
-	for _, event := range state.Events {
-		itemView := EventsView{
-			ID:          event.ID,
-			Description: event.Description,
-			Owner:       event.Collective.Name,
-			Public:      event.Public,
-			StartAt:     event.StartAt,
-		}
-		view.Events = append(view.Events, itemView)
-	}
-	return view
-}
-
-func EventDetailFromState(state *state.State, ID string) *EventDetailView {
-	event, ok := state.Events(ID)
-	if !ok {
-		return nil
-	}
-	view := EventDetailView{
-		StartAt:      event.StartAt,
-		Description:  event.Description,
-		Collective:   event.Collective.Name,
-		EstimatedEnd: event.EstimatedEnd,
-		Venue:        event.Venue,
-		Open:         event.Open,
-		Public:       event.Public,
-		Managers:     event.Managers,
-	}
-	return &view
-}
-
 // State
 
 func NewStateView(state *state.State) *StateView {
 	templates := make(map[string]*template.Template)
+
+	// Board
+	if t, err := template.ParseFiles("./api/templates/boards.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["boards"] = t
+	}
+	if eventTemplate, err := template.ParseFiles("./api/templates/board.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["board"] = eventTemplate
+	}
+
 	// Collectives
 	if collectivesTemplate, err := template.ParseFiles("./api/templates/collectives.html"); err != nil {
 		log.Fatal(err)
@@ -184,6 +312,31 @@ func NewStateView(state *state.State) *StateView {
 	} else {
 		templates["collective"] = collectiveTemplate
 	}
+
+	// Drafts
+	if draftsTemplate, err := template.ParseFiles("./api/templates/drafts.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["drafts"] = draftsTemplate
+	}
+	if draftTemplate, err := template.ParseFiles("./api/templates/draft.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["draft"] = draftTemplate
+	}
+
+	// Events
+	if t, err := template.ParseFiles("./api/templates/events.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["events"] = t
+	}
+	if eventTemplate, err := template.ParseFiles("./api/templates/event.html"); err != nil {
+		log.Fatal(err)
+	} else {
+		templates["members"] = eventTemplate
+	}
+
 	// Members
 	if t, err := template.ParseFiles("./api/templates/members.html"); err != nil {
 		log.Fatal(err)
