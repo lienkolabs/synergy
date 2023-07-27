829f04c19ee1c1d792f8af09080bbf7936bb6db5
lienko <larigdtk@gmail.com>
2023-07-27 01:07:05 -0300

    plurals, creation and update templates

---
+++ api/static/functions.js
@@ -0,0 +1,71 @@
+console.log("larissa");
+
+
+{/* <script type="text/javascript">
+  function teste() {
+      // this will get the full URL at the address bar
+      var url = window.location.href;
+      console.log(url);
+      // passes on every "a" tag
+      $(".navbar a").each(function() {
+          // checks if its the same on the address bar
+          if (url == (this.href)) {
+              $(this).closest("li").addClass("active");
+              //for making parent of submenu active
+              // $(this).closest("li").parent().parent().addClass("active");
+          }
+      });
+  };        
+</script> */}
+
+// window.onload = function() {
+//     var items = document.querySelectorAll("navitem");
+//     for (var i=0; i<items.lenght; i++) {
+//         items[i].onclick = function() {
+//             document.querySelector(this.href[1]).classList.toggle("active")
+//             return false;
+//         }
+//     }
+// }
+
+// $(".listen, .stop").click(function() {
+//     if ($(this).hasClass("listen")) {
+//         $(this).removeClass("listen").addClass("stop");
+//     } else {
+//         $(this).removeClass("stop").addClass("listen");
+//     }
+//   });
+
+
+// console.log("teste");
+// function select() {
+
+//     if(window.location.href.)
+// }
+
+
+// var navbar = document.getElementById("navbar");
+// var items = navbar.getElementsByClassName("navitem");
+// for (var i = 0; i < items.length; i++) {
+// items[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//     event.preventDefault();
+//     alert("perform action");
+// });
+// }
+
+
+{/* <script>
+  window.onload = function() {
+    var items = document.querySelectorAll("navitem");
+    for (var i=0; i<items.lenght; i++) {
+        items[i].onclick = function() {
+            document.querySelector(this.href[1]).classList.toggle("active");
+            console.log("here")
+            return false;
+        }
+    }
+  }
+</script> */}
